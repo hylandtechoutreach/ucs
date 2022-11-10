66a1c86234df3ca5f7de9e1631052996b531bbbe
hylandtechoutreach <hylandtechoutreach@users.noreply.github.com>
2022-11-10 14:35:49 +0000

    Update gitbook 2022-11-10 14:35:49

--- gitbook/gitbook-plugin-scripts/1da52bc7ee1145ae57489ed8003b0555-setTitle.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  gitbook.events.bind("page.change", function() {
-      if (gitbook.state.filepath === "BOOKREADME.md") {
-        document.title = "Hyland Academy @ UCS";
-      }
-    });
-});
